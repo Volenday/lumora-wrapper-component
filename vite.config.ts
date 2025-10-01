2f443f4ffd736629e0fddfeb1e12658570ba7996
Karla Vargas <karla.vargas@volenday.com>
2025-10-01 17:30:23 +0800

    Sets up React with Vite and TypeScript
    Initializes a React component project with Vite and TypeScript.
    
    This commit sets up the basic project structure, including:
    - Vite configuration for React and TypeScript.
    - Initial React component and associated CSS.
    - ESLint configuration.
    - Git ignore file.
    
    This provides a foundation for developing React components
    using modern tooling and best practices.

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'NovaWrapperComponent',
+      formats: ['es', 'umd'],
+      fileName: (format) => `nova-wrapper-component.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+})
